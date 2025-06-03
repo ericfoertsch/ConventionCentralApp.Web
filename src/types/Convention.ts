0f64f020d3658385e9de7ca87ef12e0aab0d889f
Eric Foertsch <foertsce@gmail.com>
2025-06-02 21:50:56 -0400

    Update the convention model.

--- src/types/Convention.ts
+++ src/types/Convention.ts
@@ -1,6 +1,17 @@
 export interface Convention {
-    id: number,
-    name: string,
-    description: string | null,
+    description: string;
+    email: string;
+    id_convention: number;
+    id_convention_series: number;
+    id_image: number;
+    id_person: number;
+    id_status: number;
+    id_venue: number;
+    id_video: number;
+    link: string;
+    name: string;
+    utc_begin: string;
+    utc_end: string;
+    utc_timestamp: string;
     logo: string | undefined
 }
